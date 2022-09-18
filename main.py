d12107b3e3f4664f6f01d8a8aa81604deb1a00a1
Chanel Cheng <chanelchengc@gmail.com>
2022-09-18 10:30:46 -0400

    Fixed to work on both M1 and CUDA

--- main.py
+++ main.py
@@ -35,9 +35,9 @@ cv = 3
 
 if __name__ == "__main__":
     # this ensures that the current MacOS version is at least 12.3+
-    print(torch.backends.mps.is_available())
+    # print(torch.backends.mps.is_available())
     # this ensures that the current current PyTorch installation was built with MPS activated.
-    print(torch.backends.mps.is_built())
+    # print(torch.backends.mps.is_built())
     print(device)
 
     # preparing data
